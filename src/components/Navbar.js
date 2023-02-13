f48085ff652e83626171b44a0bd52fb2281bd148
Vaibhav Thorat <vaibhavthorat141997@gmail.com>
2023-02-14 02:31:47 +0530

    onclick added to go to '/'

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  let navigate = useNavigate();
+
+  function handleClick() {
+    navigate("/");
+  }
   return (
     <div className=" text-lg shadow-xl py-5 px-[100px]  drop-shadow-xl">
       <div className="flex justify-between ">
         <div className="flex gap-9">
           <div className="flex text-yellow-600">
             <span class="material-icons self-center">directions_bus</span>
-            <button>RESERVE</button>
+            <button onClick={handleClick}>RESERVE</button>
           </div>
           <button>Ticket</button>
           <button>Contact Us</button>
         </div>
         <div className="flex gap-9 ">
-          <button className="bg-yellow-600 py-1 px-10 text-white rounded-xl">Login</button>
+          <button className="bg-yellow-600 py-1 px-10 text-white rounded-xl">
+            Login
+          </button>
           <button>Register</button>
         </div>
       </div>
