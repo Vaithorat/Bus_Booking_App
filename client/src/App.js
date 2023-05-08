714c22bbf5984fd73f05185f247e2a674df5c1ed
Vaibhav Thorat <vaibhavthorat141997@gmail.com>
2023-05-08 16:45:37 +0530

    finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,28 @@
-import "./App.css";
-import BusDisplay from "./pages/BusDisplay";
-import { Home } from "./pages/Home";
-import Info from "./pages/Info";
-import Payment from "./pages/Payment";
-import { Routes, Route } from "react-router-dom";
+import './App.css';
+import { useState } from 'react';
+import {Route,Routes} from "react-router-dom"
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import HomePage from './Pages/Home_Page';
+import AvailableBusPage from './Pages/Available_bus_page';
+import InfoPage from './Pages/Info_page';
+import PaymentPage from './Pages/Payment_page';
+
 function App() {
+  const [authentication, setAuthentication] = useState(false)
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/bus-display" element={<BusDisplay />} />
-        <Route path="/info" element={<Info />} />
-        <Route path="/info/payment" element={<Payment />} />
-      </Routes>
-    </>
+    <div className="App">
+      <Header authentication={authentication} setAuthentication={setAuthentication} />
+      <main >
+        <Routes>
+          <Route path='/' element={<HomePage authentication={authentication} setAuthentication={setAuthentication} />} />
+          <Route path='/availablebus' element={<AvailableBusPage />} />
+          <Route path='/info' element={<InfoPage />} />
+          <Route path='/payment' element={<PaymentPage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
--- client/src/components/ui/Button/Button.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "../../../styles/UI/ui.css";
-
-export const Button = ({ name }) => {
-  return <button className="ui-btn">{name}</button>;
-};
